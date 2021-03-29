b29e211b919835b20d94f255fdc48a78e242f095
Pablo <pabritos@gmail.com>
2021-03-29 17:01:08 -0300

    Iteración con 1000 puntos

--- juego-del-caos.m
+++ juego-del-caos.m
@@ -8,8 +8,10 @@
 % realicé es la siguiente:
 %
 
-clc
+clc; 
+
 clear
+
 % Considero el triángulo isósceles, ABC
 % Donde A=[0,0] , B=[1,0] y C=[0.5,sqrt(3)/2]
 
@@ -25,11 +27,12 @@ else
   y = (1-x)*sqrt(3)/2*rand;
 end
 
+# Consigo todos los puntos a graficar
 for i=1:n
   % Elijo un vértice aleatorio
   V=randi([0 2]);    
   switch V
-    case 0 % Punto medio entre A y P   
+    case 0 % Punto medio entre A y P
         X_z = (x)/2;
         Y_z = (y)/2;
     case 1 % Punto medio entre B y P
@@ -45,11 +48,34 @@ for i=1:n
   % Sigo a partir del punto obtenido
   x = X_z;
   y = Y_z;  
-end 
+end
 
-scatter(X,Y,10)
-%plot(X,Y,'.')
-title('Triángulo de Sierpinski’s con Octave - Pablo Britos')
-legend(sprintf('n=%d Iteraciones',n))
+# Graficamos los primeros 500 con pausa
+tic
+for i=1:1000
+  plot(X(i),Y(i),'.',...
+      'MarkerSize',10,...
+      'MarkerEdgeColor','b') 
+  hold on
+  if i < 300
+    pause(0.01)
+  else
+    pause(0.001)
+  end
+  if i < 2 
+    # Las siguientes instrucciones las ejecutamos una 
+    # sola vez ya que tardan medio segundo cada vez.
+    axis([0 1 0 sqrt(3)/2*0.5])
+    title('El juego del caos con Octave - Pablo Britos')
+    legend(sprintf('n=%d Iteraciones',n))  
+  end
+end
+toc 
+# Graficamos los restantes sin pausa ya que
+# representarlos uno a uno en un bucle tarda mucho.
 
-
+tic 
+plot(X,Y,'.',...
+      'MarkerSize',5,...
+      'MarkerEdgeColor','b')
+toc
